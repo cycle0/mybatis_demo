6850bafa16b42f69c9f9da94864c1efa09fe308f
fct01 <37719002+fct01@users.noreply.github.com>
2019-11-20 15:44:03 +0800

    添加用户功能开发

---
+++ src/main/java/com/rock/mybatis_demo/mapper/UserMapper.java
@@ -0,0 +1,89 @@
+package com.rock.mybatis_demo.mapper;
+
+import com.rock.mybatis_demo.model.User;
+import com.rock.mybatis_demo.model.UserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.DeleteProvider;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.InsertProvider;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectProvider;
+import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.annotations.UpdateProvider;
+import org.apache.ibatis.type.JdbcType;
+
+public interface UserMapper {
+    @SelectProvider(type=UserSqlProvider.class, method="countByExample")
+    int countByExample(UserExample example);
+
+    @DeleteProvider(type=UserSqlProvider.class, method="deleteByExample")
+    int deleteByExample(UserExample example);
+
+    @Delete({
+        "delete from user",
+        "where id = #{id,jdbcType=BIGINT}"
+    })
+    int deleteByPrimaryKey(Long id);
+
+    @Insert({
+        "insert into user (name, age, ",
+        "email, deleted)",
+        "values (#{name,jdbcType=VARCHAR}, #{age,jdbcType=INTEGER}, ",
+        "#{email,jdbcType=VARCHAR}, #{deleted,jdbcType=INTEGER})"
+    })
+    @Options(useGeneratedKeys=true,keyProperty="id")
+    int insert(User record);
+
+    @InsertProvider(type=UserSqlProvider.class, method="insertSelective")
+    @Options(useGeneratedKeys=true,keyProperty="id")
+    int insertSelective(User record);
+
+    @SelectProvider(type=UserSqlProvider.class, method="selectByExample")
+    @Results({
+        @Result(column="id", property="id", jdbcType=JdbcType.BIGINT, id=true),
+        @Result(column="name", property="name", jdbcType=JdbcType.VARCHAR),
+        @Result(column="age", property="age", jdbcType=JdbcType.INTEGER),
+        @Result(column="email", property="email", jdbcType=JdbcType.VARCHAR),
+        @Result(column="deleted", property="deleted", jdbcType=JdbcType.INTEGER)
+    })
+    List<User> selectByExample(UserExample example);
+
+    @Select({
+        "select",
+        "id, name, age, email, deleted",
+        "from user",
+        "where id = #{id,jdbcType=BIGINT}"
+    })
+    @Results({
+        @Result(column="id", property="id", jdbcType=JdbcType.BIGINT, id=true),
+        @Result(column="name", property="name", jdbcType=JdbcType.VARCHAR),
+        @Result(column="age", property="age", jdbcType=JdbcType.INTEGER),
+        @Result(column="email", property="email", jdbcType=JdbcType.VARCHAR),
+        @Result(column="deleted", property="deleted", jdbcType=JdbcType.INTEGER)
+    })
+    User selectByPrimaryKey(Long id);
+
+    @UpdateProvider(type=UserSqlProvider.class, method="updateByExampleSelective")
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    @UpdateProvider(type=UserSqlProvider.class, method="updateByExample")
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+
+    @UpdateProvider(type=UserSqlProvider.class, method="updateByPrimaryKeySelective")
+    int updateByPrimaryKeySelective(User record);
+
+    @Update({
+        "update user",
+        "set name = #{name,jdbcType=VARCHAR},",
+          "age = #{age,jdbcType=INTEGER},",
+          "email = #{email,jdbcType=VARCHAR},",
+          "deleted = #{deleted,jdbcType=INTEGER}",
+        "where id = #{id,jdbcType=BIGINT}"
+    })
+    int updateByPrimaryKey(User record);
+}
---
+++ src/main/java/com/rock/mybatis_demo/model/User.java
@@ -0,0 +1,68 @@
+package com.rock.mybatis_demo.model;
+
+public class User {
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+
+    private String email;
+
+    private Integer deleted;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Integer deleted) {
+        this.deleted = deleted;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", age=").append(age);
+        sb.append(", email=").append(email);
+        sb.append(", deleted=").append(deleted);
+        sb.append("]");
+        return sb.toString();
+    }
+}
